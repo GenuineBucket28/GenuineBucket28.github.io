dcdd2e8d1289d8cc13ba75c4827c0a0d133cba01
GenuineBucket28 <cyberknight028@gmail.com>
2021-05-10 14:07:47 -0600

    I added a JS loop

--- scripts.js
+++ scripts.js
@@ -1,10 +1,18 @@
 function testFunction(num1, num2) {
-  return num1 + num2
+  return num1 + num2;
 }
-var num = 0
+var num = 0;
 function counterUp() {
-  return num += 1
+  return num += 1;
 }
 function counterDown() {
-  return num -= 1
+  return num -= 1;
+}
+var text = ''
+var i
+function loopTest() {
+  for (i = 0; i < 10; i++) {
+    text += "This loop has ran" + i + "time(s)<br>"
+  }
+  return text
 }
